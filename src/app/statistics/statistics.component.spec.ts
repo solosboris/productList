a2b89d2918c00f7af5150cd9a75b7ce542dbb81b
solosboris <solosboris@gmail.com>
2020-03-15 23:37:03 +0100

    testTask: added the new test cases

--- src/app/statistics/statistics.component.spec.ts
+++ src/app/statistics/statistics.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ProductsService } from '../services/products/products.service';
 import { StatisticsComponent } from './statistics.component';
 
 describe('StatisticsComponent', () => {
@@ -8,9 +9,10 @@ describe('StatisticsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ProductsService],
       declarations: [ StatisticsComponent ]
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +23,35 @@ describe('StatisticsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#1 should getBoldestBrandLess40', () => {
+    component.ngOnInit();
+    expect(component.topBrandLess40 !== 'no size less 40').toBeTruthy();
+  });
+
+  it('#2 should getBoldestBrandLess40', () => {
+    component.ngOnInit();
+    expect(component.topBrandLess40.indexOf(':') > 0).toBeTruthy();
+  });
+
+  it('#1 should getSizes', () => {
+    component.ngOnInit();
+    expect(component.sizes !== 'no sizes mapping').toBeTruthy();
+  });
+
+  it('#2 should getSizes', () => {
+    component.ngOnInit();
+    expect(component.sizes.indexOf('max sized') == 0).toBeTruthy();
+  });
+
+  it('#1 should getPriceSize32', () => {
+    component.ngOnInit();
+    expect(component.priceSize32 !== 'no for 32nd sizes').toBeTruthy();
+  });
+
+  it('#2 should getPriceSize32', () => {
+    component.ngOnInit();
+    expect(component.priceSize32.indexOf(':') > 0).toBeTruthy();
   });
 });
